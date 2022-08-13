2922632a01d88c81280018a5ea5e52d25dd348bc
Saifullakh1 <saifullakh7@gmail.com>
2022-08-13 14:22:38 +0600

    work with api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Ccomponent from './Ccomponent';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Ccomponent numbers={[1, 2, 3, 4]}/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Web Developer Blog
         </p>
         <a
           className="App-link"
@@ -15,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Web Developer Blog
         </a>
       </header>
     </div>
